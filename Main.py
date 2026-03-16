76be84f836008792b94cc90f7db7dcfbed0b0525
Diabi <zuza.rykaczewska@gmail.com>
2026-03-16 23:24:49 +0100

    Optymalizacja makaronu

--- Main.py
+++ Main.py
@@ -1,108 +1,79 @@
 import random
 from collections import Counter
 
-Ruch_Gracz1 = 0
-Wynik_Gracz1 = 0
-Ruch_Gracz2 = 0
-Wynik_Gracz2 = 0
-
-K1_Gracz1 = [None, None, None]
-K2_Gracz1 = [None, None, None]
-K3_Gracz1 = [None, None, None]
-K1_Gracz2 = [None, None, None]
-K2_Gracz2 = [None, None, None]
-K3_Gracz2 = [None, None, None]
 
 def policz_wynik(kolumny):
     total = 0
     for kol in kolumny:
         liczby = [x for x in kol if x is not None]
-        liczby_licznik = Counter(liczby)
-        for liczba, ile_razy in liczby_licznik.items():
-            total += liczba * ile_razy * ile_razy  # <-- każda kostka mnożona przez ilość powtórzeń
+        licznik = Counter(liczby)
+
+        for liczba, ile in licznik.items():
+            total += liczba * ile * ile
+
     return total
 
-while True:
-    '''
-    GRACZ 1
-    '''
+
+def wstaw_kostke(kolumny, kolumna, oczka):
+    kol = kolumny[kolumna]
+
+    for i in range(3):
+        if kol[i] is None:
+            kol[i] = oczka
+            return
+
+    print("Brak możliwości położenia tam kostki")
+
+
+def plansza_pelna(kolumny):
+    return None not in sum(kolumny, [])
+
+
+def tura(gracz, kolumny):
     print()
 
-    Oczka_Gracz1 = random.randint(1, 6)
-    print("Gracz 1 wylosował: ", Oczka_Gracz1)
+    oczka = random.randint(1, 6)
+    print(f"Gracz {gracz} wylosował:", oczka)
 
-    Ruch_Gracz1 = int(input("Gdzie położyć kostkę? (kolumna 0-3): "))
+    ruch = int(input("Gdzie położyć kostkę? (kolumna 1-3): ")) - 1
 
-    if Ruch_Gracz1 == 1:
-        if K1_Gracz1[0] is None: K1_Gracz1[0] = Oczka_Gracz1
-        elif K1_Gracz1[1] is None: K1_Gracz1[1] = Oczka_Gracz1
-        elif K1_Gracz1[2] is None: K1_Gracz1[2] = Oczka_Gracz1
-        else: print("Brak możliwości położenia tam kostki")
-    if Ruch_Gracz1 == 2:
-        if K2_Gracz1[0] is None: K2_Gracz1[0] = Oczka_Gracz1
-        elif K2_Gracz1[1] is None: K2_Gracz1[1] = Oczka_Gracz1
-        elif K2_Gracz1[2] is None: K2_Gracz1[2] = Oczka_Gracz1
-        else: print("Brak możliwości położenia tam kostki")
-    if Ruch_Gracz1 == 3:
-        if K3_Gracz1[0] is None: K3_Gracz1[0] = Oczka_Gracz1
-        elif K3_Gracz1[1] is None: K3_Gracz1[1] = Oczka_Gracz1
-        elif K3_Gracz1[2] is None: K3_Gracz1[2] = Oczka_Gracz1
-        else: print("Brak możliwości położenia tam kostki")
+    if 0 <= ruch < 3:
+        wstaw_kostke(kolumny, ruch, oczka)
 
-    print(K1_Gracz1)
-    print(K2_Gracz1)
-    print(K3_Gracz1)
+    for kol in kolumny:
+        print(kol)
 
-    Kolumny_Gracz1 = [K1_Gracz1, K2_Gracz1, K3_Gracz1]
-    Wynik_Gracz1 = policz_wynik(Kolumny_Gracz1)
-    print("Gracz 1: ", Wynik_Gracz1, " pkt")
+    wynik = policz_wynik(kolumny)
+    print(f"Gracz {gracz}: {wynik} pkt")
+
+    return wynik
 
 
-    if None not in K1_Gracz1 + K2_Gracz1 + K3_Gracz1:
-        print("\nKoniec gry!")
-        if Wynik_Gracz1 > Wynik_Gracz2: print("Wygrywa gracz 1!")
-        elif Wynik_Gracz1 < Wynik_Gracz2: print("Wygrywa gracz 2!")
-        else: print("Remis!")
+kolumny_gracz1 = [[None]*3 for _ in range(3)]
+kolumny_gracz2 = [[None]*3 for _ in range(3)]
+
+wynik1 = 0
+wynik2 = 0
+
+
+while True:
+
+    wynik1 = tura(1, kolumny_gracz1)
+
+    if plansza_pelna(kolumny_gracz1):
         break
-    else:
-        '''
-        GRACZ 2
-        '''
 
-        print()
+    wynik2 = tura(2, kolumny_gracz2)
 
-        Oczka_Gracz2 = random.randint(1, 6)
-        print("Gracz 2 wylosował: ", Oczka_Gracz2)
+    if plansza_pelna(kolumny_gracz2):
+        break
 
-        Ruch_Gracz2 = int(input("Gdzie położyć kostkę? (kolumna 0-3): "))
 
-        if Ruch_Gracz2 == 1:
-            if K1_Gracz2[0] is None: K1_Gracz2[0] = Oczka_Gracz2
-            elif K1_Gracz2[1] is None: K1_Gracz2[1] = Oczka_Gracz2
-            elif K1_Gracz2[2] is None: K1_Gracz2[2] = Oczka_Gracz2
-            else: print("Brak możliwości położenia tam kostki")
-        if Ruch_Gracz2 == 2:
-            if K2_Gracz2[0] is None: K2_Gracz2[0] = Oczka_Gracz2
-            elif K2_Gracz2[1] is None: K2_Gracz2[1] = Oczka_Gracz2
-            elif K2_Gracz2[2] is None: K2_Gracz2[2] = Oczka_Gracz2
-            else: print("Brak możliwości położenia tam kostki")
-        if Ruch_Gracz2 == 3:
-            if K3_Gracz2[0] is None: K3_Gracz2[0] = Oczka_Gracz2
-            elif K3_Gracz2[1] is None: K3_Gracz2[1] = Oczka_Gracz2
-            elif K3_Gracz2[2] is None: K3_Gracz2[2] = Oczka_Gracz2
-            else: print("Brak możliwości położenia tam kostki")
+print("\nKoniec gry!")
 
-        print(K1_Gracz2)
-        print(K2_Gracz2)
-        print(K3_Gracz2)
-
-        Kolumny_Gracz2 = [K1_Gracz2, K2_Gracz2, K3_Gracz2]
-        Wynik_Gracz2 = policz_wynik(Kolumny_Gracz2)
-        print("Gracz 2: ", Wynik_Gracz2, " pkt")
-
-        if None not in K1_Gracz1 + K2_Gracz1 + K3_Gracz1:
-            print("\nKoniec gry!")
-            if Wynik_Gracz1 > Wynik_Gracz2: print("Wygrywa gracz 1!")
-            elif Wynik_Gracz1 < Wynik_Gracz2: print("Wygrywa gracz 2!")
-            else: print("Remis!")
-            break
+if wynik1 > wynik2:
+    print("Wygrywa gracz 1!")
+elif wynik1 < wynik2:
+    print("Wygrywa gracz 2!")
+else:
+    print("Remis!")
