a6de756a11143840562698bd2e23c80e34b33f94
Rykaczewska <zuzanna.rykaczewska@accenture.com>
2026-08-26 12:06:23 +0200

    Translated to eng

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ import random
 from collections import Counter
 
 
-def policz_wynik(kolumny):
+def calculate_score(columns):
     total = 0
-    for kol in kolumny:
+    for kol in columns:
         liczby = [x for x in kol if x is not None]
         licznik = Counter(liczby)
 
@@ -14,8 +14,8 @@ def policz_wynik(kolumny):
     return total
 
 
-def wstaw_kostke(kolumny, kolumna, oczka):
-    kol = kolumny[kolumna]
+def wstaw_kostke(columns, column, oczka):
+    kol = columns[column]
 
     for i in range(3):
         if kol[i] is None:
@@ -25,32 +25,32 @@ def wstaw_kostke(kolumny, kolumna, oczka):
     print("Brak możliwości położenia tam kostki")
 
 
-def plansza_pelna(kolumny):
-    return None not in sum(kolumny, [])
+def plansza_pelna(columns):
+    return None not in sum(columns, [])
 
 
-def tura(gracz, kolumny):
+def tura(gracz, columns):
     print()
 
     oczka = random.randint(1, 6)
     print(f"Gracz {gracz} wylosował:", oczka)
 
-    ruch = int(input("Gdzie położyć kostkę? (kolumna 1-3): ")) - 1
+    ruch = int(input("Gdzie położyć kostkę? (column 1-3): ")) - 1
 
     if 0 <= ruch < 3:
-        wstaw_kostke(kolumny, ruch, oczka)
+        wstaw_kostke(columns, ruch, oczka)
 
-    for kol in kolumny:
+    for kol in columns:
         print(kol)
 
-    wynik = policz_wynik(kolumny)
+    wynik = policz_wynik(columns)
     print(f"Gracz {gracz}: {wynik} pkt")
 
     return wynik
 
 
-kolumny_gracz1 = [[None]*3 for _ in range(3)]
-kolumny_gracz2 = [[None]*3 for _ in range(3)]
+columns_gracz1 = [[None]*3 for _ in range(3)]
+columns_gracz2 = [[None]*3 for _ in range(3)]
 
 wynik1 = 0
 wynik2 = 0
@@ -58,14 +58,14 @@ wynik2 = 0
 
 while True:
 
-    wynik1 = tura(1, kolumny_gracz1)
+    wynik1 = tura(1, columns_gracz1)
 
-    if plansza_pelna(kolumny_gracz1):
+    if plansza_pelna(columns_gracz1):
         break
 
-    wynik2 = tura(2, kolumny_gracz2)
+    wynik2 = tura(2, columns_gracz2)
 
-    if plansza_pelna(kolumny_gracz2):
+    if plansza_pelna(columns_gracz2):
         break
 
 
